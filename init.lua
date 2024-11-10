84082e7e7ba8a4592d3981d9faf9ab14debec575
Delici0u-s <maxi.bravo111@gmail.com>
2024-11-10 16:27:39 +0100

    asd

--- init.lua
+++ init.lua
@@ -208,6 +208,80 @@ require('lazy').setup({
       vim.cmd 'colorscheme kanagawa-dragon'
     end,
   },
+  { 'nvim-neotest/nvim-nio' },
+
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'leoluz/nvim-dap-go',
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+    },
+    config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
+
+      require('dapui').setup { icons = { expanded = '▾', collapsed = '▸', current_frame = '★' } }
+      require('dap-go').setup()
+
+      -- Configure C++ adapter for `cppdbg`
+      dap.adapters.cppdbg = {
+        id = 'cppdbg',
+        type = 'executable',
+        command = 'C:/Users/maxib/.vscode/extensions/ms-vscode.cpptools-1.22.11-win32-x64/debugAdapters/bin/OpenDebugAD7.exe',
+        options = { detached = false },
+      }
+
+      dap.configurations.cpp = {
+        {
+          name = 'Launch file',
+          type = 'cppdbg',
+          request = 'launch',
+          program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          end,
+          cwd = '${workspaceFolder}',
+          stopAtEntry = true,
+          setupCommands = {
+            {
+              text = '-enable-pretty-printing',
+              description = 'Enable pretty-printing',
+              ignoreFailures = false,
+            },
+          },
+        },
+      }
+
+      -- Keybindings and UI setup (same as before)
+      vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
+      vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
+      vim.keymap.set('n', '<space>?', function()
+        require('dapui').eval(nil, { enter = true })
+      end)
+      vim.keymap.set('n', '<F1>', dap.continue)
+      vim.keymap.set('n', '<F2>', dap.step_into)
+      vim.keymap.set('n', '<F3>', dap.step_over)
+      vim.keymap.set('n', '<F4>', dap.step_out)
+      vim.keymap.set('n', '<F5>', dap.step_back)
+      vim.keymap.set('n', '<F13>', dap.restart)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
+
   -- Remaining plugins
   {
     'nvim-neo-tree/neo-tree.nvim',
